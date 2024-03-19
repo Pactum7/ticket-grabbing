9b6701c4b6dc6460d656d13fda5a806fa5343e4d
nuoyan <nuoyan@douyu.tv>
2024-03-19 17:34:37 +0800

    格式化代码

--- MaoYan/MaoYanGoNew.js
+++ MaoYan/MaoYanGoNew.js
@@ -3,7 +3,7 @@
 //  //打开猫眼app
 //  app.launchApp("猫眼");
 openConsole();
-console.setTitle("猫眼 go!","#ff11ee00",30);
+console.setTitle("猫眼 go!", "#ff11ee00", 30);
 
 //统计绝对坐标
 //关闭Alert弹窗坐标
@@ -20,166 +20,166 @@ const closeTicketRegisterX = 942;
 const closeTicketRegisterY = 997;
 //四排票档坐标
 const ticketBtnArr = [
-    [215,1030],[505,1080],[830,1080],
-    [215,1250],[505,1250],[830,1250],
-    [215,1400],[505,1400],[830,1400],
-    [215,1620],[505,1620],[830,1620]
+    [215, 1030], [505, 1080], [830, 1080],
+    [215, 1250], [505, 1250], [830, 1250],
+    [215, 1400], [505, 1400], [830, 1400],
+    [215, 1620], [505, 1620], [830, 1620]
 ];
 
 main();
 
 //获取输入的场次信息
-function getPlayEtc(){
-   var playEtc = rawInput("请输入场次关键字(按照默认格式)", "周六");
-   if (playEtc == null || playEtc.trim()=='') {
-       alert("请输入场次信息!");
-       return getPlayEtc();
-   }
-   console.log("手动输入的场次信息："+playEtc);
-   return playEtc;
+function getPlayEtc() {
+    var playEtc = rawInput("请输入场次关键字(按照默认格式)", "周六");
+    if (playEtc == null || playEtc.trim() == '') {
+        alert("请输入场次信息!");
+        return getPlayEtc();
+    }
+    console.log("手动输入的场次信息：" + playEtc);
+    return playEtc;
 }
 
 //获取输入票价信息
-function getTicketPrice(){
-   var ticketPrice = rawInput("请输入票价关键字(按照默认格式)", "380");
-   if (ticketPrice == null || ticketPrice.trim()=='') {
-       alert("请输入票价信息!");
-       return getTicketPrice();
+function getTicketPrice() {
+    var ticketPrice = rawInput("请输入票价关键字(按照默认格式)", "380");
+    if (ticketPrice == null || ticketPrice.trim() == '') {
+        alert("请输入票价信息!");
+        return getTicketPrice();
     }
 
-   console.log("手动输入的票价信息："+ticketPrice);
-   return ticketPrice;
+    console.log("手动输入的票价信息：" + ticketPrice);
+    return ticketPrice;
 }
 
 //获取输入的抢票时间
-function getSellTime(){
-   var sellTime = rawInput("请输入抢票时间(按照默认格式)", "03-19 15:00");
-   if (sellTime == null || sellTime.trim()=='') {
-       alert("请输入抢票时间!");
-       return getSellTime();
+function getSellTime() {
+    var sellTime = rawInput("请输入抢票时间(按照默认格式)", "03-19 15:00");
+    if (sellTime == null || sellTime.trim() == '') {
+        alert("请输入抢票时间!");
+        return getSellTime();
     }
-   return sellTime;
+    return sellTime;
 }
 
 
 function main() {
-   console.log("开始猫眼抢票!");
-   var preBook= text("已 预 约").findOne(2000)
-   var preBook2 = className("android.widget.TextView").text("已填写").findOne(2000)
-   var isPreBook = preBook2!=null||preBook!=null;
-   var playEtc;
-   var ticketPrice;
-   console.log("界面是否已预约："+isPreBook);
-   if(!isPreBook){
-       console.log("无预约信息，请输入抢票信息!");
-       playEtc = getPlayEtc();
-       ticketPrice = getTicketPrice();
-   }
+    console.log("开始猫眼抢票!");
+    var preBook = text("已 预 约").findOne(2000)
+    var preBook2 = className("android.widget.TextView").text("已填写").findOne(2000)
+    var isPreBook = preBook2 != null || preBook != null;
+    var playEtc;
+    var ticketPrice;
+    console.log("界面是否已预约：" + isPreBook);
+    if (!isPreBook) {
+        console.log("无预约信息，请输入抢票信息!");
+        playEtc = getPlayEtc();
+        ticketPrice = getTicketPrice();
+    }
 
-   var month;
-   var day;
-   var hour;
-   var minute;
+    var month;
+    var day;
+    var hour;
+    var minute;
 
-   var inputTime = getSellTime();
-   //在这里使用输入的时间进行后续操作
-   console.log("输入的抢票时间：" + inputTime);
-   var times = inputTime.split(" ");
-   var time1 = times[0]
-   var time2 = times[1]
-   var monthDay= time1.split("-");
-   month = monthDay[0] - 1;
-   day = monthDay[1];
-   var hourMinute= time2.split(":");
-   hour = hourMinute[0];
-   minute=  hourMinute[1];
+    var inputTime = getSellTime();
+    //在这里使用输入的时间进行后续操作
+    console.log("输入的抢票时间：" + inputTime);
+    var times = inputTime.split(" ");
+    var time1 = times[0]
+    var time2 = times[1]
+    var monthDay = time1.split("-");
+    month = monthDay[0] - 1;
+    day = monthDay[1];
+    var hourMinute = time2.split(":");
+    hour = hourMinute[0];
+    minute = hourMinute[1];
 
-   // 设置开抢时间
-   var year = new Date().getFullYear();
-   var second = 0;
-   var msecond = 0;
-   var startTimestamp = new Date(year, month, day, hour, minute, second, msecond).getTime();
+    // 设置开抢时间
+    var year = new Date().getFullYear();
+    var second = 0;
+    var msecond = 0;
+    var startTimestamp = new Date(year, month, day, hour, minute, second, msecond).getTime();
     // 减去 45ms 的网络延迟
-   startTimestamp = startTimestamp - 45;
-   var damaiTimestamp;
-   var startTime = convertToTime(startTimestamp);
-   console.log("开抢时间：", startTime);
-   console.log("等待开抢...");
-   // 循环等待
-   while (true) {
-       damaiTimestamp = getDamaiTimestamp();
+    startTimestamp = startTimestamp - 45;
+    var damaiTimestamp;
+    var startTime = convertToTime(startTimestamp);
+    console.log("开抢时间：", startTime);
+    console.log("等待开抢...");
+    // 循环等待
+    while (true) {
+        damaiTimestamp = getDamaiTimestamp();
 
-       if (damaiTimestamp >= startTimestamp) {
-           break;
-       }
-   }
-
-   var realStartTime = getDamaiTimestamp();
-   console.log("冲啊！！！");
-   while(true){
-       var but1 = classNameStartsWith('android.widget.').text("立即预订").exists();
-       var but2 = classNameStartsWith('android.widget.').text("立即购票").exists();
-       var but3= classNameStartsWith('android.widget.').text("特惠购票").exists();
-       //var but4= classNameStartsWith('android.widget.').text("缺货登记").exists();
-       var result = but1||but2||but3;
-       if(result){
-           var s;
-           if(but1){
-               var s =classNameStartsWith('android.widget.').text("立即预订").findOne().click();
-           }else if(but2){
-               var s =classNameStartsWith('android.widget.').text("立即购票").findOne().click();
-           }else if(but3){
-               var s =classNameStartsWith('android.widget.').text("特惠购票").findOne().click();
-           }
-           console.log("点击了立即购票相关按钮："+s)
-           break;
-       }
-   }
-   if(!isPreBook){
-        //等待主区域加载出来
-        textContains("请选择票档").waitFor();
-       
-       // 选择场次
-       //textContains(playEtc).findOne().parent().click();
-       // textContains(" "+playEtc+" ").waitFor();
-       // textContains(" "+playEtc+" ").findOne().click();
-       // console.log("选择场次");
-       // ticketPrice = "¥"+ticketPrice
-       
-
-       //如果票档区域没加载出来就执行刷新 TODO 调试的时候开启
-       if(!textContains("看台").exists()){
-            refresh_ticket_dom();
-       }
-       textContains("看台").waitFor();
-       textContains(ticketPrice).findOne().click();
-       console.log("选择票档");
-       textContains("数量").waitFor();
-   }
-   //className("android.widget.TextView").text("确认").waitFor();
-   // classNameStartsWith('android.widget.').text("确认").findOne().click();
-    for(let cnt = 0; cnt >= 0; cnt++){
-        //猛点，一直点到出现支付按钮为止
-        click(ConfirmX,ConfirmY);
-        sleep(50);
-        if(className("android.widget.Button").exists()){
+        if (damaiTimestamp >= startTimestamp) {
             break;
         }
-        if(cnt % 20 == 0){
-            log("已点击确认次数："+cnt);
+    }
+
+    var realStartTime = getDamaiTimestamp();
+    console.log("冲啊！！！");
+    while (true) {
+        var but1 = classNameStartsWith('android.widget.').text("立即预订").exists();
+        var but2 = classNameStartsWith('android.widget.').text("立即购票").exists();
+        var but3 = classNameStartsWith('android.widget.').text("特惠购票").exists();
+        //var but4= classNameStartsWith('android.widget.').text("缺货登记").exists();
+        var result = but1 || but2 || but3;
+        if (result) {
+            var s;
+            if (but1) {
+                var s = classNameStartsWith('android.widget.').text("立即预订").findOne().click();
+            } else if (but2) {
+                var s = classNameStartsWith('android.widget.').text("立即购票").findOne().click();
+            } else if (but3) {
+                var s = classNameStartsWith('android.widget.').text("特惠购票").findOne().click();
+            }
+            console.log("点击了立即购票相关按钮：" + s)
+            break;
         }
     }
-    
-    for(let cnt = 0; className("android.widget.Button").exists(); cnt++){
+    if (!isPreBook) {
+        //等待主区域加载出来
+        textContains("请选择票档").waitFor();
+
+        // 选择场次
+        //textContains(playEtc).findOne().parent().click();
+        // textContains(" "+playEtc+" ").waitFor();
+        // textContains(" "+playEtc+" ").findOne().click();
+        // console.log("选择场次");
+        // ticketPrice = "¥"+ticketPrice
+
+
+        //如果票档区域没加载出来就执行刷新 TODO 调试的时候开启
+        if (!textContains("看台").exists()) {
+            refresh_ticket_dom();
+        }
+        textContains("看台").waitFor();
+        textContains(ticketPrice).findOne().click();
+        console.log("选择票档");
+        textContains("数量").waitFor();
+    }
+    //className("android.widget.TextView").text("确认").waitFor();
+    // classNameStartsWith('android.widget.').text("确认").findOne().click();
+    for (let cnt = 0; cnt >= 0; cnt++) {
+        //猛点，一直点到出现支付按钮为止
+        click(ConfirmX, ConfirmY);
+        sleep(50);
+        if (className("android.widget.Button").exists()) {
+            break;
+        }
+        if (cnt % 20 == 0) {
+            log("已点击确认次数：" + cnt);
+        }
+    }
+
+    for (let cnt = 0; className("android.widget.Button").exists(); cnt++) {
         //直接猛点就完事了
         var c = className("android.widget.Button").findOne().click();
         sleep(50);
-        if(cnt % 20 == 0){
-            log("已点击支付次数："+cnt);
+        if (cnt % 20 == 0) {
+            log("已点击支付次数：" + cnt);
         }
     }
-   
-   console.log("结束时间："+convertToTime(getDamaiTimestamp()))
+
+    console.log("结束时间：" + convertToTime(getDamaiTimestamp()))
 
 
 }
@@ -189,17 +189,17 @@ function main() {
 * @returns 大麦服务器时间戳
 */
 function getDamaiTimestamp() {
-   return JSON.parse(http.get("https://mtop.damai.cn/gw/mtop.common.getTimestamp/", {
-       headers: {
-           'Host': 'mtop.damai.cn',
-           'Content-Type': 'application/json;charset=utf-8',
-           'Accept': '*/*',
-           'User-Agent': 'floattime/1.1.1 (iPhone; iOS 15.6; Scale/3.00)',
-           'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
-           'Accept-Encoding': 'gzip, deflate, br',
-           'Connection': 'keep-alive'
-       }
-   }).body.string()).data.t;
+    return JSON.parse(http.get("https://mtop.damai.cn/gw/mtop.common.getTimestamp/", {
+        headers: {
+            'Host': 'mtop.damai.cn',
+            'Content-Type': 'application/json;charset=utf-8',
+            'Accept': '*/*',
+            'User-Agent': 'floattime/1.1.1 (iPhone; iOS 15.6; Scale/3.00)',
+            'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive'
+        }
+    }).body.string()).data.t;
 }
 
 /**
@@ -208,33 +208,33 @@ function getDamaiTimestamp() {
 * @returns ISO 8601 格式的北京时间
 */
 function convertToTime(timestamp) {
-   var date = new Date(Number(timestamp));
-   var year = date.getUTCFullYear();
-   var month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
-   var day = date.getUTCDate().toString().padStart(2, "0");
-   var hours = (date.getUTCHours() + 8).toString().padStart(2, "0");
-   var minutes = date.getUTCMinutes().toString().padStart(2, "0");
-   var seconds = date.getUTCSeconds().toString().padStart(2, "0");
-   var milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0");
-   var iso8601 = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-   return iso8601;
+    var date = new Date(Number(timestamp));
+    var year = date.getUTCFullYear();
+    var month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    var day = date.getUTCDate().toString().padStart(2, "0");
+    var hours = (date.getUTCHours() + 8).toString().padStart(2, "0");
+    var minutes = date.getUTCMinutes().toString().padStart(2, "0");
+    var seconds = date.getUTCSeconds().toString().padStart(2, "0");
+    var milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0");
+    var iso8601 = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+    return iso8601;
 }
 
-function refresh_dom(){
-    threads.start(function(){
+function refresh_dom() {
+    threads.start(function () {
         sleep(20)
         //点确定关闭alert弹窗
-        click(closeAlertX,closeAlertY);
+        click(closeAlertX, closeAlertY);
     });
     // rawInput("请输入场次关键字(按照默认格式)", "周六");
     alert("刷新dom!");
     log("alert刷新dom")
 }
 
-function refresh_ticket_dom(){
-    for(let i=0;i<ticketBtnArr.length;i++){
+function refresh_ticket_dom() {
+    for (let i = 0; i < ticketBtnArr.length; i++) {
         click(ticketBtnArr[i][0], ticketBtnArr[i][1]);
-        if(textContains('登记号码').exists()){
+        if (textContains('登记号码').exists()) {
             click(closeTicketRegisterX, closeTicketRegisterY);
             console.log("成功刷新dom");
             return;
@@ -247,13 +247,13 @@ function refresh_ticket_dom(){
 
 //点击控件所在坐标
 function btn_position_click(x) {
-   if (x) {
-      var b = x.bounds();
-      print(b.centerX())
-      print(b.centerY())
-      var c = click(b.centerX(), b.centerY()) 
+    if (x) {
+        var b = x.bounds();
+        print(b.centerX())
+        print(b.centerY())
+        var c = click(b.centerX(), b.centerY())
 
-      console.log("点击是否成功："+c);
-   }
+        console.log("点击是否成功：" + c);
+    }
 }
 
--- MaoYan/MaoYanMonitor.js
+++ MaoYan/MaoYanMonitor.js
@@ -1,6 +1,6 @@
 
 openConsole();
-console.setTitle("余票监控 go!","#ff11ee00",30);
+console.setTitle("余票监控 go!", "#ff11ee00", 30);
 
 //统计绝对坐标
 //关闭Alert弹窗坐标
@@ -17,10 +17,10 @@ const closeTicketRegisterX = 942;
 const closeTicketRegisterY = 997;
 //四排票档坐标
 const ticketBtnArr = [
-    [215,1030],[505,1080],[830,1080],
-    [215,1250],[505,1250],[830,1250],
-    [215,1400],[505,1400],[830,1400],
-    [215,1620],[505,1620],[830,1620]
+    [215, 1030], [505, 1080], [830, 1080],
+    [215, 1250], [505, 1250], [830, 1250],
+    [215, 1400], [505, 1400], [830, 1400],
+    [215, 1620], [505, 1620], [830, 1620]
 ];
 
 //监控时间间隔，单位：秒
@@ -30,27 +30,27 @@ main();
 
 
 //获取输入票价信息
-function getMaxTicketPrice(){
+function getMaxTicketPrice() {
     var ticketPrice = rawInput("请输入监控最高票价", "1000");
-    if (ticketPrice == null || ticketPrice.trim()=='') {
+    if (ticketPrice == null || ticketPrice.trim() == '') {
         alert("请输入监控最高票价!");
         return getMaxTicketPrice();
     }
 
-   
-   return ticketPrice;
+
+    return ticketPrice;
 }
 
 
 function main() {
     var maxTicketPrice = getMaxTicketPrice();
-    console.log("监控最高票价："+maxTicketPrice);
+    console.log("监控最高票价：" + maxTicketPrice);
 
-    if(!textContains("看台").exists()){
+    if (!textContains("看台").exists()) {
         refresh_ticket_dom();
     }
-    
-    while(true){
+
+    while (true) {
         cycleMonitor(maxTicketPrice);
         //每5秒刷新一次票档
         sleep(monitorIntervalSeconds * 1000);
@@ -58,59 +58,59 @@ function main() {
         textMatches(/202\d\-\d+\-\d+ 周.*/).findOne().click();
         sleep(1000)
     }
-   
-   
+
+
 
 }
 
-function cycleMonitor(maxTicketPrice){
+function cycleMonitor(maxTicketPrice) {
     //等待余票信息加载出来
     textContains("请选择票档").waitFor();
     //获取符合条件的票档数组
     let targetTickets = get_less_than_tickets(maxTicketPrice)
-    for(let amount of targetTickets){
-        log("开冲一个："+amount);
+    for (let amount of targetTickets) {
+        log("开冲一个：" + amount);
         //返回true表示走完了流程还没抢到，需要刷新票档，返回false表示没确认成功，更换一个票档继续尝试确认
-        if(doSubmit(amount)){
+        if (doSubmit(amount)) {
             break;
         }
     }
 }
 
-function doSubmit(amount){
-    
-    textContains("¥"+amount).findOne().click();
+function doSubmit(amount) {
+
+    textContains("¥" + amount).findOne().click();
     textContains("数量").waitFor();
-    if(text("1份").exists()){
+    if (text("1份").exists()) {
         //点+1
-        click(pulsOneX,pulsOneY);
+        click(pulsOneX, pulsOneY);
     }
 
     let attemptCnt = 0;
     let attemptMaxCnt = 150;
-    while(className("android.widget.TextView").text("确认").exists() && attemptMaxCnt <= attemptMaxCnt){
+    while (className("android.widget.TextView").text("确认").exists() && attemptMaxCnt <= attemptMaxCnt) {
         // 点击确认
         // if(text("确认").exists()){
-            text("确认").click();
-            // log('点确认按钮');
+        text("确认").click();
+        // log('点确认按钮');
         // }else{
-            click(ConfirmX,ConfirmY);
+        click(ConfirmX, ConfirmY);
         //     log('点确认坐标');
         // }
-        if(className("android.widget.Button").exists()){
+        if (className("android.widget.Button").exists()) {
             break;
         }
         attemptCnt++;
     }
-    if(attemptCnt >= attemptMaxCnt && !className("android.widget.Button").exists()){
+    if (attemptCnt >= attemptMaxCnt && !className("android.widget.Button").exists()) {
         return false;
     }
-    log("尝试次数："+attemptCnt);
+    log("尝试次数：" + attemptCnt);
 
     //等待立即支付按钮出现
     className("android.widget.Button").waitFor();
     //没有选择观演人时，把所有没选的都选上
-    if(text("wc0GRRGh2f2pQAAAABJRU5ErkJggg==").exists()){
+    if (text("wc0GRRGh2f2pQAAAABJRU5ErkJggg==").exists()) {
         text("wc0GRRGh2f2pQAAAABJRU5ErkJggg==").click();
     }
     // //点击支付
@@ -123,18 +123,18 @@ function doSubmit(amount){
     //     var c = className("android.widget.Button").findOne().click();
     //     console.log("继续点击立即支付 "+c);  
     // }
-    console.log("准备点击 "); 
-    for(let cnt = 0; className("android.widget.Button").exists(); cnt++){
+    console.log("准备点击 ");
+    for (let cnt = 0; className("android.widget.Button").exists(); cnt++) {
         //直接猛点就完事了
         var c = className("android.widget.Button").findOne().click();
         sleep(50);
-        if(cnt % 20 == 0){
-            log("已点击支付次数："+cnt);
+        if (cnt % 20 == 0) {
+            log("已点击支付次数：" + cnt);
         }
         //TODO 出现类似【票已经卖完了】退出循环，继续刷新票档
     }
-    
-    console.log("结束时间："+convertToTime(getDamaiTimestamp()))
+
+    console.log("结束时间：" + convertToTime(getDamaiTimestamp()))
     return true;
 }
 
@@ -143,17 +143,17 @@ function doSubmit(amount){
 * @returns 大麦服务器时间戳
 */
 function getDamaiTimestamp() {
-   return JSON.parse(http.get("https://mtop.damai.cn/gw/mtop.common.getTimestamp/", {
-       headers: {
-           'Host': 'mtop.damai.cn',
-           'Content-Type': 'application/json;charset=utf-8',
-           'Accept': '*/*',
-           'User-Agent': 'floattime/1.1.1 (iPhone; iOS 15.6; Scale/3.00)',
-           'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
-           'Accept-Encoding': 'gzip, deflate, br',
-           'Connection': 'keep-alive'
-       }
-   }).body.string()).data.t;
+    return JSON.parse(http.get("https://mtop.damai.cn/gw/mtop.common.getTimestamp/", {
+        headers: {
+            'Host': 'mtop.damai.cn',
+            'Content-Type': 'application/json;charset=utf-8',
+            'Accept': '*/*',
+            'User-Agent': 'floattime/1.1.1 (iPhone; iOS 15.6; Scale/3.00)',
+            'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive'
+        }
+    }).body.string()).data.t;
 }
 
 /**
@@ -162,25 +162,25 @@ function getDamaiTimestamp() {
 * @returns ISO 8601 格式的北京时间
 */
 function convertToTime(timestamp) {
-   var date = new Date(Number(timestamp));
-   var year = date.getUTCFullYear();
-   var month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
-   var day = date.getUTCDate().toString().padStart(2, "0");
-   var hours = (date.getUTCHours() + 8).toString().padStart(2, "0");
-   var minutes = date.getUTCMinutes().toString().padStart(2, "0");
-   var seconds = date.getUTCSeconds().toString().padStart(2, "0");
-   var milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0");
-   var iso8601 = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-   return iso8601;
+    var date = new Date(Number(timestamp));
+    var year = date.getUTCFullYear();
+    var month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    var day = date.getUTCDate().toString().padStart(2, "0");
+    var hours = (date.getUTCHours() + 8).toString().padStart(2, "0");
+    var minutes = date.getUTCMinutes().toString().padStart(2, "0");
+    var seconds = date.getUTCSeconds().toString().padStart(2, "0");
+    var milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0");
+    var iso8601 = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+    return iso8601;
 }
 /**
  * alert一个弹窗，提前使用异步线程延时点击关闭alert弹窗
  */
-function refresh_dom(){
-    threads.start(function(){
+function refresh_dom() {
+    threads.start(function () {
         sleep(20)
         //点确定关闭alert弹窗
-        click(closeAlertX,closeAlertY);
+        click(closeAlertX, closeAlertY);
     });
     // rawInput("请输入场次关键字(按照默认格式)", "周六");
     alert("刷新dom!");
@@ -192,10 +192,10 @@ function refresh_dom(){
  * 排票档的绝对坐标，依次点击触发“缺票登记”弹窗再关闭即可；
  * 兜底情况alert一个弹窗，提前使用异步线程延时点击关闭alert弹窗
  */
-function refresh_ticket_dom(){
-    for(let i=0;i<ticketBtnArr.length;i++){
+function refresh_ticket_dom() {
+    for (let i = 0; i < ticketBtnArr.length; i++) {
         click(ticketBtnArr[i][0], ticketBtnArr[i][1]);
-        if(textContains('登记号码').exists()){
+        if (textContains('登记号码').exists()) {
             click(closeTicketRegisterX, closeTicketRegisterY);
             console.log("成功刷新dom");
             return;
@@ -205,11 +205,11 @@ function refresh_ticket_dom(){
     refresh_dom();
 }
 
-function get_less_than_tickets(maxTicketPrice){
+function get_less_than_tickets(maxTicketPrice) {
     var targetTickets = [];
-    textContains("¥").find().forEach(function(btn){
+    textContains("¥").find().forEach(function (btn) {
         // log(btn.text());
-        if(!btn.text().includes("缺货登记")){
+        if (!btn.text().includes("缺货登记")) {
             let match = btn.text().match(/\¥(\d+)/);
             let amount;
             if (match && (amount = parseInt(match[1])) < maxTicketPrice) {
@@ -218,22 +218,22 @@ function get_less_than_tickets(maxTicketPrice){
 
         }
     });
-    targetTickets.sort(function(a, b) {
+    targetTickets.sort(function (a, b) {
         return a - b;
     });
-    log("符合条件:"+targetTickets);
+    log("符合条件:" + targetTickets);
     return targetTickets;
 }
 
 //点击控件所在坐标
 function btn_position_click(x) {
-   if (x) {
-      var b = x.bounds();
-      print(b.centerX())
-      print(b.centerY())
-      var c = click(b.centerX(), b.centerY()) 
+    if (x) {
+        var b = x.bounds();
+        print(b.centerX())
+        print(b.centerY())
+        var c = click(b.centerX(), b.centerY())
 
-      console.log("点击是否成功："+c);
-   }
+        console.log("点击是否成功：" + c);
+    }
 }
 
