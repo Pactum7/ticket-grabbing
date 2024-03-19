d9f0c0ff8ea647b3ab645504fa53bdc0653647a2
Clearlove77777777777 <45119228+Clearlove77777777777@users.noreply.github.com>
2024-03-20 00:50:09 +0800

    通用化改造

--- Test/Test.js
+++ Test/Test.js
@@ -1,1 +1,13 @@
-log(className("android.widget.Button").exists())
+// const closeAlertX = 873;
+// const closeAlertY = 1321;
+
+// // threads.start(function () {
+// //     sleep(200)
+// //     //点确定关闭alert弹窗
+// //     click(closeAlertX, closeAlertY);
+// // });
+// // rawInput("请输入场次关键字(按照默认格式)", "周六");
+// alert("刷新dom!");
+
+
+toast("测试");
